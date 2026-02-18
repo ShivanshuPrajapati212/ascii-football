0e2485a18fc0f2ef7de91e92c129834aa92d65a8
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-02-18 10:04:23 +0530

    add loop in that with 24 fps

--- internal/loop.go
+++ internal/loop.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/ShivanshuPrajapati212/ascii-football/ascii"
 	"golang.org/x/term"
 )
+
+const FPS int = 24
 
 func MainLoop() {
 	width, height, err := term.GetSize(int(os.Stdout.Fd()))
@@ -17,5 +20,11 @@ func MainLoop() {
 	if width < 120 || height < 30 {
 		log.Fatal("Need bigger terminal, 120x30")
 	}
-	fmt.Print(ascii.FootballFieldASCII)
+	for {
+		startTime := time.Now()
+		fmt.Print("\033[2J\033[0;0H")
+		fmt.Print(ascii.FootballFieldASCII)
+		time.Sleep(time.Duration((1.0/float32(FPS))*float32(time.Second)) - time.Since(startTime))
+
+	}
 }
